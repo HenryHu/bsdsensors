9862935ce97481ea6fc86959d34af79a83bb6a1f
Henry Hu <henry.hu.sh@gmail.com>
2018-04-30 23:37:34 -0400

    dumpable interface

--- util.h
+++ util.h
@@ -9,6 +9,12 @@
 #define __BSDSENSORS_UTIL_H__
 
 #include <cstdint>
+#include <iostream>
+
+class DumpAble {
+   public:
+    virtual void DumpInfo(std::ostream& out) = 0;
+};
 
 inline uint16_t Combine(uint8_t high, uint8_t low) {
     return ((uint16_t)high << 8) + low;
