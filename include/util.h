d85839ce0e2de0ad5050b71ce29cc1358cf0b656
Henry Hu <henry.hu.sh@gmail.com>
2018-05-03 23:57:38 -0400

    bit operations

--- include/util.h
+++ include/util.h
@@ -20,4 +20,22 @@ inline uint16_t Combine(uint8_t high, uint8_t low) {
     return ((uint16_t)high << 8) + low;
 }
 
+// Some bits in a byte
+// [4:0] -> first: 4 width: 5
+struct Bits {
+    int first, last;
+
+    // Full byte
+    Bits() : first(7), last(0) {}
+    // One-bit flag
+    Bits(int bit) : first(bit), last(bit) {}
+    // General case
+    Bits(int first, int last) : first(first), last(last) {}
+
+    bool full() const { return first - last + 1 == 8; }
+};
+
+extern uint8_t BitsFromByte(const Bits& bits, uint8_t byte);
+extern uint8_t BitsToByte(const Bits& bits, uint8_t byte, uint8_t value);
+
 #endif  // __BSDSENSORS_UTIL_H__
---
+++ lib/util.cc
@@ -0,0 +1,35 @@
+/*
+ * util.cc
+ * Copyright (C) 2018 Henry Hu
+ *
+ * Distributed under terms of the 3-clause BSD license.
+ */
+
+#include "util.h"
+
+// Return @bits in @byte.
+uint8_t BitsFromByte(const Bits& bits, uint8_t byte) {
+    assert(bits.first <= 8);
+    assert(bits.last >= 0);
+    assert(bits.first >= bits.last);
+
+    if (bits.first - bits.last + 1 == 8) {
+        return byte;
+    }
+
+    return (byte >> bits.last) & ((1 << (bits.first - bits.last + 1)) - 1);
+}
+
+// Return @byte with @bits replaced by @value.
+uint8_t BitsToByte(const Bits& bits, uint8_t byte, uint8_t value) {
+    assert(bits.first <= 8);
+    assert(bits.last >= 0);
+    assert(bits.first >= bits.last);
+
+    if (bits.first - bits.last + 1 == 8) {
+        return value;
+    }
+
+    uint8_t mask = (1 << (bits.first - bits.last + 1)) - 1;
+    return (byte & ~(mask << bits.last)) | ((value & mask) << bits.last);
+}
