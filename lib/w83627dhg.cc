c051ed7f1a9836fae4f219fa701ff8d1d9222e72
Henry Hu <henry.hu.sh@gmail.com>
2025-01-20 16:22:15 -0500

    typo

--- lib/w83627dhg.cc
+++ lib/w83627dhg.cc
@@ -21,7 +21,7 @@ std::map<NuvotonTempSource, uint8_t> kW83627DHGTempSource{
 };
 
 const NuvotonChipInfo kW83627DHG = {
-    .device_id_to_name = {{0xA025, "W83627DGH"}},
+    .device_id_to_name = {{0xA025, "W83627DHG"}},
     .vendor_id_addr = {0, 0x4F},
     .fans = {
         {
