c0d7efa29b8074b3fd5deb0dd9ba09a95f8b043b
Henry Hu <henry.hu.sh@gmail.com>
2025-01-20 17:04:14 -0500

    update test infra

---
+++ tests/nuvoton_test.h
@@ -0,0 +1,13 @@
+/*
+ * nuvoton_test.h
+ * Copyright (C) 2018 Henry Hu
+ *
+ * Distributed under terms of the 3-clause BSD license.
+ */
+
+#ifndef __BSDSENSORS_TEST_NUVOTON_TEST_H_
+#define __BSDSENSORS_TEST_NUVOTON_TEST_H_
+
+#include <gtest/gtest.h>
+
+#endif // __BSDSENSORS_TEST_NUVOTON_TEST_H_
---
+++ tests/w83627hgaw_test.cc
@@ -0,0 +1,8 @@
+#include "nuvoton_chip.h"
+#include "nuvoton_test.h"
+
+class W83627HGAWTest : public ::testing::Test {
+};
+
+TEST_F(W83627HGAWTest, Basic) {
+}
