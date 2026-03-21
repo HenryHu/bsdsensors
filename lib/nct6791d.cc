057993e3b483c64fd7e23fea1401a570176a2a9b
spin24 <spin.tw@gmail.com>
2026-03-21 18:33:55 +0800

    Add support for NCT6791D revision 0xC803 (ASUS H97-PLUS)

--- lib/nct6791d.cc
+++ lib/nct6791d.cc
@@ -39,7 +39,7 @@ std::map<NuvotonTempSource, uint8_t> kNCT6791DTempSource{
 };
 
 const NuvotonChipInfo kNCT6791D = {
-    .device_id_to_name = {{0xC800, "NCT6791D"}},
+    .device_id_to_name = {{0xC800, "NCT6791D"}, {0xC803, "NCT6791D"}},
     .vendor_id_addr = {0, 0x4F},
     // Fan speed info
     .fans = {
